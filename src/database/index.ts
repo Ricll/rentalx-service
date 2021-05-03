52db9b296e9fa4a0a1db2c50fce96b67d7044249
RickDev <ricllima@hotmail.com>
2021-05-03 16:36:00 -0300

    first commit

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,5 +1,6 @@
 import { createConnection, getConnectionOptions } from "typeorm";
 
+import User from "../modules/accounts/entities/User";
 import Category from "../modules/cars/entities/Category";
 import Specification from "../modules/cars/entities/Specification";
 
@@ -11,7 +12,7 @@ getConnectionOptions().then(options => {
   const newOptions = options as IOptions;
   newOptions.host = "database"; // Essa opção deverá ser EXATAMENTE o nome dado ao service do banco de dados
   createConnection({
-    entities: [Category, Specification],
+    entities: [User, Category, Specification],
     ...options,
   });
 });
--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -15,7 +15,7 @@ class ImportCategoryUseCase {
     private categoriesRepository: ICategoriesRepository,
   ) {}
 
-  loadCategories(file: Express.Multer.File): Promise<IImportCategory[]> {
+  async loadCategories(file: Express.Multer.File): Promise<IImportCategory[]> {
     return new Promise((resolve, reject) => {
       const stream = fs.createReadStream(file.path);
       const categories: IImportCategory[] = [];
