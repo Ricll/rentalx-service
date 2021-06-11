a300f8e0fbc0b996e4ff0183b4fc79806c37cc49
RickDev <ricllima@hotmail.com>
2021-06-11 19:51:40 -0300

    first commit

--- dist/media/rickdev/a342c7ca-9636-4ab8-b760-cd8d561188ce1/home/rick/ROCKETSEAT 2021/rentalx/src/shared/container/providers/DateProvider/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var tsyringe_1 = require("tsyringe");
-var DayJsDateProvider_1 = require("./implementations/DayJsDateProvider");
-tsyringe_1.container.registerSingleton("DayJsDateProvider", DayJsDateProvider_1.DayJsDateProvider);
